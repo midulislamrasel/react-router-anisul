bcd2ba771d91d845f9f7a664e42112f7d5f40721
Midul-islam-Russell <midulraddy121@gmail.com>
2023-01-08 11:44:10 +0600

    add Error

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from '../About/About';
+import Error from '../Error/Error';
 import Footer from '../Footer/Footer';
 import Home from '../Home/Home';
 import Order from '../Order/Order';
@@ -13,10 +14,10 @@ const Main = () => {
                     <Route path='/home' element ={<Home></Home>}></Route>
                     <Route path='/about' element ={<About></About>}></Route>
                     <Route path='/order' element ={<Order></Order>}></Route>
-                    <Route path='/footer' element ={<Footer></Footer>}></Route>
+                    <Route path='/footer' element={<Footer></Footer>}></Route>
+                    <Route path = '*' element ={<Error></Error>}></Route>
                 </Routes>
             </BrowserRouter>
-            <h3>Mani page Fille</h3>
         </div>
     );
 };
